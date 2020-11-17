b647a7b7964c9bbdeea5ea3b0c03c7a12db32903
tatiana <khen.tatiana@gmail.com>
2020-11-17 15:39:08 -0800

    Event synopsis layout is done.

--- ParentAid/EventCell.swift
+++ ParentAid/EventCell.swift
@@ -15,8 +15,7 @@ class EventCell: UITableViewCell {
     @IBOutlet weak var photoViewProvider: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
+           }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
