dd8100b235a657e42bae17537a5ae72d72a4662f
tatiana <khen.tatiana@gmail.com>
2020-11-19 02:04:23 -0800

    SignUpVC is done

--- ParentAid/LoginViewController.swift
+++ ParentAid/LoginViewController.swift
@@ -45,44 +45,12 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func onSignUp(_ sender: Any) {
-        var user = PFUser(className: "_User")
-             user.username = usernameField.text
-             user.password = passwordField.text
-      
-         let email = isValidEmail(testStr: usernameField.text!)
-
-        if email == false{
-            showAlert(title: "Invalid", message: "This is not a valid email. Please try again.")
-            usernameField.text = ""
+        self.performSegue(withIdentifier: "signupSegue", sender: nil)
         }
-
-
-        if (user.password!.count < 5)
-        {
-            let alert = UIAlertController(title: "Invalid password", message: "Password must be greater than 5 characters.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            present(alert, animated: true)
-        }
-        
-             user.signUpInBackground{ (success, error) in
-                 if success {
-                     self.performSegue(withIdentifier: "loginSegue", sender: nil)
-                 } else {
-                     print ("Error: \(error?.localizedDescription)")
-                 }
-     }
-
-    }
     
-    func isValidEmail(testStr:String) -> Bool {
-            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-            
-            let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-            return emailTest.evaluate(with: testStr)
-        }
     func showAlert(title: String, message: String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
             self.present(alert, animated: true, completion: nil)
-        }
+   }
 }
--- ParentAid/SignUpViewController.swift
+++ ParentAid/SignUpViewController.swift
@@ -6,24 +6,83 @@
 //
 
 import UIKit
+import Parse
 
 class SignUpViewController: UIViewController {
-
+    
+    @IBOutlet weak var passwordSignupField: UITextField!
+    @IBOutlet weak var emailSignupField: UITextField!
+    @IBOutlet weak var usernameSignupField: UITextField!
+    
+    var actInd = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupActivityIndicator()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupActivityIndicator(){
+        actInd.center = self.view.center
+        actInd.hidesWhenStopped = true
+        actInd.style = UIActivityIndicatorView.Style.large
+        actInd.color = UIColor.blue
+        
+        view.addSubview(actInd)
     }
-    */
-
+    
+    
+    @IBAction func onSignUpAction(_ sender: Any) {
+        var user = PFUser(className: "_User")
+        user.username = usernameSignupField.text
+        user.email = emailSignupField.text
+        user.password = passwordSignupField.text
+        
+        let email = isValidEmail(testStr: emailSignupField.text!)
+        
+        if email == false{
+            showAlert(title: "Invalid", message: "This is not a valid email. Please try again.")
+            emailSignupField.text = ""
+        }
+        
+        
+        if (user.username!.count < 5) || (user.password!.count < 5)
+        {
+            let alert = UIAlertController(title: "Invalid username or password", message: "Username or password must be greater than 5 characters.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true)
+        }
+        
+        user.signUpInBackground{ (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginFromSignup", sender: nil)
+            } else {
+                print ("Error: \(error?.localizedDescription)")
+                self.showAlert(title: "Invalid", message: "Invalid username or email or password. Please try again.")
+            }
+        }
+        
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
