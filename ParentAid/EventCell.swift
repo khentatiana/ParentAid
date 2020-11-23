40de338dce8a0eed134bbe06f8b3ccc743478bf4
tatiana <khen.tatiana@gmail.com>
2020-11-23 13:12:02 -0800

    before merging with master branch

--- ParentAid/EventCell.swift
+++ ParentAid/EventCell.swift
@@ -13,6 +13,10 @@ class EventCell: UITableViewCell {
     @IBOutlet weak var synopsisLabel: UILabel!
     @IBOutlet weak var providerLabel: UILabel!
     @IBOutlet weak var photoViewProvider: UIImageView!
+    
+    // Add Event variable + didset
+    var events = [PFObject]()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
            }
