c5f473502cc78ff1c6ae6b2a6236045266d16ebe
tatiana <khen.tatiana@gmail.com>
2020-12-04 00:42:08 -0800

    Created MapVC

---
+++ ParentAid/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  ParentAid
+//
+//  Created by Tatiana on 12/4/20.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
