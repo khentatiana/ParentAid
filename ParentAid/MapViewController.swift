fb6206fd766b50068430355fa0d5a3eb54080001
tatiana <khen.tatiana@gmail.com>
2020-12-04 12:00:16 -0800

    need to redo

--- ParentAid/MapViewController.swift
+++ ParentAid/MapViewController.swift
@@ -16,6 +16,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager = CLLocationManager()
     var currentLocation : CLLocation?
+    var isUpdatingLocation = false
+    var lastLocationError: Error?
     var latitude : String?
     var longitude : String?
     var address : String?
@@ -34,21 +36,55 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
             return
         }
-        
+        //Start and stop finding location
         // If location services is enabled get the users location
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuracy here.
-            locationManager.startUpdatingLocation()
+        if isUpdatingLocation{
+            stopLocationManager()
+        } else {
+            //set last locatrion to nil to start fresh
+            currentLocation = nil
+            lastLocationError = nil
+            startLocationManager()
+        }
+        updateUI()
+    }
+        
+    func updateUI(){
+        if let currentLocation = currentLocation{
+            //TODO: pupulate the location with coordinate info
+            
+            
+        } else{
+            latitude = ""
+            longitude = ""
+            address = ""
         }
     }
     
-    // Print out the location to the console
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            print(location.coordinate)
-        }
+
+func startLocationManager(){
+    if CLLocationManager.locationServicesEnabled() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuracy here.
+        locationManager.startUpdatingLocation()
+        isUpdatingLocation = true
     }
+}
+func stopLocationManager(){
+    if isUpdatingLocation{
+        locationManager.stopUpdatingLocation()
+        locationManager.delegate = nil
+        isUpdatingLocation = false
+    }
+}
+
+
+    // Print out the location to the console
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        if let location = locations.first {
+//            print(location.coordinate)
+//        }
+//    }
     
     // If we have been deined access give the user the option to change it
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -77,16 +113,20 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         self.present(alertController, animated: true, completion: nil)
     }
     
-    func updateUI(){
-        if let currentLocation = currentLocation{
-            //TODO: pupulate the location with coordinate info
-            
-            
-        } else{
-            latitude = ""
-            longitude = ""
-            address = ""
+   
+  
+        func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print ("Error !!! locationManager didFailWithError: \(error)")
+            if (error as NSError).code == CLError.locationUnknown.rawValue{
+                return
+            }
+            lastLocationError = error
+            stopLocationManager()
+            updateUI()
         }
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+            if let location = locations.first {
+                print(location.coordinate)
+            }
     }
-    
 }
