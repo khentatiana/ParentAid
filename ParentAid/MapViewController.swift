511587c7467051c6d63ae8a9a24387cedfaf2cce
tatiana <khen.tatiana@gmail.com>
2020-12-04 03:06:56 -0800

    user current location address

--- ParentAid/MapViewController.swift
+++ ParentAid/MapViewController.swift
@@ -11,98 +11,19 @@ import CoreLocation
 
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
-//    let locationManager = CLLocationManager()
-//    var currentCoordinate: CLLocationCoordinate2D?
-//
+
 @IBOutlet weak var mapView: MKMapView!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        locationManagerDidChangeAuthorization(locationManager)
-//        // Do any additional setup after loading the view.
-//    }
-//
-////    private func configureLocationServices() {
-////        locationManager.delegate = self
-////        let status = CLLocationManager.authorizationStatus()
-////
-////        if status == .notDetermined {
-////            locationManager.requestAlwaysAuthorization()
-////        } else if status == .authorizedAlways || status == .authorizedWhenInUse {
-////           beginLocationUpdates(locationManager: locationManager)
-////        }
-////    }
-//    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-//        locationManager.delegate = self
-//        if #available(iOS 14.0, *) {
-//               switch locationManager.authorizationStatus {
-//                   case .authorizedAlways , .authorizedWhenInUse:
-//                       break
-//                   case .notDetermined , .denied , .restricted:
-//                       break
-//                   default:
-//                       break
-//               }
-//
-//               switch locationManager.accuracyAuthorization {
-//                   case .fullAccuracy:
-//                       break
-//                   case .reducedAccuracy:
-//                       break
-//                   default:
-//                       break
-//               }
-//           }
-//    }
-//
-//    func beginLocationUpdates(locationManager: CLLocationManager) {
-//        mapView.showsUserLocation = true
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        locationManager.startUpdatingLocation()
-//    }
-//
-//     func zoomToLatestLocation(with coordinate: CLLocationCoordinate2D) {
-//        let zoomRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
-//        mapView.setRegion(zoomRegion, animated: true)
-//    }
-//}
-//
-//extension MapViewController: CLLocationManagerDelegate {
-//
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        print("Did get latest location")
-//
-//        guard let latestLocation = locations.first else { return }
-//
-//        if currentCoordinate == nil {
-//            zoomToLatestLocation(with: latestLocation.coordinate)
-//        }
-//
-//        currentCoordinate = latestLocation.coordinate
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-//       print("The status changed")
-//        if status == .authorizedAlways || status == .authorizedWhenInUse {
-//            beginLocationUpdates(locationManager: manager)
-//        }
-//    }
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
+
     let locationManager = CLLocationManager()
-    var currentLocation : CLLocation!
+    var currentLocation : CLLocation?
+    var latitude : String?
+    var longitude : String?
+    var address : String?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         
         
         // For use when the app is open & in the background
@@ -136,7 +57,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     // Show the popup to the user if we have been deined access
     func showLocationDisabledPopUp() {
         let alertController = UIAlertController(title: "Background Location Access Disabled",
-                                                message: "In order to deliver pizza we need your location",
+                                                message: "In order to find kids events near by we need your location",
                                                 preferredStyle: .alert)
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
@@ -150,6 +71,18 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         alertController.addAction(openAction)
         
         self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func updateUI(){
+        if let currentLocation = currentLocation{
+            //TODO: pupulate the location with coordinate info
+            
+            
+        } else{
+            latitude = ""
+            longitude = ""
+            address = ""
+        }
     }
     
 }
