02f179ba58982df350363aa9c2ff090ee6156b7d
tatiana <khen.tatiana@gmail.com>
2020-12-11 14:43:29 -0800

    started RegisterParentVC

--- ParentAid/ParentKidsEventsViewController.swift
+++ ParentAid/ParentKidsEventsViewController.swift
@@ -89,6 +89,9 @@ class ParentKidsEventsViewController: UIViewController, UITableViewDelegate, UIT
         let urlString = imageFile.url!
         let url = URL(string: urlString)!
         cell.eventPictureParentProvider.af_setImage(withURL: url)
+        //To make round corners of the image
+               cell.eventPictureParentProvider.layer.cornerRadius = 10
+               cell.eventPictureParentProvider.clipsToBounds = true
 
 //        let cell = UITableViewCell()
 //        cell.textLabel?.text = "row: \(indexPath.row)"
@@ -108,6 +111,77 @@ class ParentKidsEventsViewController: UIViewController, UITableViewDelegate, UIT
         else {return}
         delegate.window?.rootViewController = loginViewController
     }
+    //dismiss keyboard by clicking outside textbox
+       override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           self.view.endEditing(true)
+       }
+
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+            //animate tableViewProviderTopConstraint
+        tableViewParentTopConstraint.constant = 0
+            UIView.animate(withDuration: 0.1) {
+                self.view.layoutIfNeeded()
+            }
+            tableViewParent.isHidden = false
+            // Show Cancel button when typing
+            self.searchBarParent.showsCancelButton = true
+        
+    }
+        // Logic for searchBar cancel button
+        func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+            searchBarParent.showsCancelButton = false // remove cancel button
+            searchBarParent.text = "" // reset search text
+            searchBarParent.resignFirstResponder() // remove keyboard
+            //filteredEvents = eventsArray // reset results to display
+            tableViewParent.reloadData()
+        }
+        
+        @objc func didCreateNewEvent(){
+            let query = PFQuery(className: "KidsEvents")
+            query.includeKeys(["provider.username", "synopsis" , "title", "date"])
+            query.order(byDescending: "createdAt")
+            query.limit = numberOfEvents
+                    
+            query.findObjectsInBackground{ (events, error) in
+                if (events != nil){
+                    self.events = events!
+                    self.tableViewParent.reloadData()
+                }
+            }
+        }
+    //MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           // Get the new view controller using segue.destination.
+         //   Pass the selected object to the new view controller.
+           print("########### Loading up the details from parent screen")
     
+          //if true || false || true
+           
+           //Find the selected event
+             if let cell = sender as? UITableViewCell  { // Sender is the cell that was tapped
+               
+               if let indexPath = tableViewParent.indexPath(for: cell){//Index of the cell was tapped
+                   let event = events[indexPath.row] //event from selected cell
+                  // let event = filteredEvents[indexPath.row] //event from selected cell
+                   
     
-}
+                   //Pass the selected movie to the details view controller
+                   //Variable "detailsViewController" is a destination where selected event is segue
+                   if let detailsViewController = segue.destination as? RegisterParentViewController{
+    
+                       detailsViewController.event = event //this "event" is referring to the selected event from ProviderKidsEventsViewController
+    
+                   //Deselect event when transitioning (after tapping and coming back to main screen)
+                       tableViewParent.deselectRow(at: indexPath, animated: true)}
+               }
+             
+           }
+               
+           
+       
+    
+       }
+   }
+
+    
+
--- ParentAid/RegisterParentViewController.swift
+++ ParentAid/RegisterParentViewController.swift
@@ -6,16 +6,51 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
 class RegisterParentViewController: UIViewController {
 
+       
+    var event : PFObject!
+    var events = [PFObject]()
+    var providerUser : PFObject!
+   var providerProfiles = [PFObject]()
+    
+//    @IBOutlet weak var eventImageView: UIImageView!
+//
+//    @IBOutlet weak var providerProfilePhoto: UIImageView!
+//    @IBOutlet weak var synopsisLabel: UILabel!
+//    @IBOutlet weak var providerName: UILabel!
+//    @IBOutlet weak var eventDate: UILabel!
+//    @IBOutlet weak var eventTitle: UILabel!
+//    @IBOutlet weak var checkboxCOVIDsafe: UILabel!
+//    @IBOutlet weak var eventRegistrants: UILabel!
+//    @IBOutlet weak var availableSpots: UILabel!
+//
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //display provider name
+//        let user = event["provider"] as! PFUser
+//        providerName.text = user.username
+//      
+//        eventTitle.text = event["title"] as? String
+//        eventTitle.sizeToFit()
+//        
+//        synopsisLabel.text = event["synopsis"] as? String
+//        synopsisLabel.sizeToFit()
+//        
+//        eventDate.text = event["date"] as? String
+//        eventDate.sizeToFit()
+//        
+//        let imageFile = event["image"] as! PFFileObject
+//        let urlString = imageFile.url!
+//        let url = URL(string: urlString)!
+//        eventImageView.af.setImage(withURL: url)
+//        //To make round corners of the image
+//        eventImageView.layer.cornerRadius = 10
+//        eventImageView.clipsToBounds = true
     }
-    
-
     /*
     // MARK: - Navigation
 
